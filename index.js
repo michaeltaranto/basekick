16c30b9c2ef79ad0a7003a062dfc449a24a8c206
michaeltaranto <michael.taranto@gmail.com>
2016-05-12 15:12:25 +1000

    Remove redundant calc

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var basekick = function (options) {
   var typeOffset = calculateTypeOffset(lineHeightScale);
 
   return {
-    fontSize: 'calc(' + options.typeSizeModifier + ' * ' + options.baseFontSize + 'px)',
+    fontSize: (options.typeSizeModifier * options.baseFontSize) + 'px',
     lineHeight: lineHeightScale + 'em',
     transform: 'translateY(' + typeOffset + 'em)'
   };
